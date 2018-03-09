562ba08770a904b97e214efae42ba16c2572b0aa
gongjian <gongjian@gongjian-rmbp>
2018-03-09 19:33:25 +0800

    liquibase

---
+++ src/main/java/org/james/liquibase/configuration/LiquibaseConfiguration.java
@@ -0,0 +1,21 @@
+package org.james.liquibase.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+public class LiquibaseConfiguration {
+	@Bean
+	public SpringLiquibase liquibase(DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(dataSource);
+		liquibase.setChangeLog("classpath:liquibase/master.xml");
+		liquibase.setContexts("development,test,production");
+		liquibase.setShouldRun(true);
+		return liquibase;
+	}
+}
